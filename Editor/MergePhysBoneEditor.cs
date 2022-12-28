e07ba36508a8c41e6a5259a351050af83c44db29
anatawa12 <anatawa12@icloud.com>
2022-12-28 16:37:54 +0900

    chore: merge Eq

--- Editor/MergePhysBoneEditor.cs
+++ Editor/MergePhysBoneEditor.cs
@@ -30,6 +30,8 @@ namespace Anatawa12.Merger
 
         public override void OnInspectorGUI()
         {
+            
+
             EditorGUILayout.LabelField("Components:");
 
             for (var i = 0; i < Target.components.Length; i++)
@@ -57,6 +59,8 @@ namespace Anatawa12.Merger
         private bool Eq(float a, float b) => Mathf.Abs(a - b) < 0.00001f;
         private bool Eq(Vector3 a, Vector3 b) => (a - b).magnitude < 0.00001f;
         private bool Eq(AnimationCurve a, AnimationCurve b) => a.Equals(b);
+        private bool Eq(float aFloat, AnimationCurve aCurve, float bFloat, AnimationCurve bCurve) => 
+            Eq(aFloat, bFloat) && Eq(aCurve, bCurve);
 
         private bool IsValidPhysBone(VRCPhysBone a)
         {
@@ -74,37 +78,27 @@ namespace Anatawa12.Merger
             // Multi Child Type: ignore: Must be 'Ignore'
             // == Forces ==
             if (a.integrationType != b.integrationType) return false;
-            if (!Eq(a.pull, b.pull)) return false;
-            if (!Eq(a.pullCurve, b.pullCurve)) return false;
-            if (!Eq(a.spring, b.spring)) return false;
-            if (!Eq(a.springCurve, b.springCurve)) return false;
-            if (!Eq(a.stiffness, b.stiffness)) return false;
-            if (!Eq(a.stiffnessCurve, b.stiffnessCurve)) return false;
-            if (!Eq(a.gravity, b.gravity)) return false;
-            if (!Eq(a.gravityCurve, b.gravityCurve)) return false;
-            if (!Eq(a.gravityFalloff, b.gravityFalloff)) return false;
-            if (!Eq(a.gravityFalloffCurve, b.gravityFalloffCurve)) return false;
+            if (!Eq(a.pull, a.pullCurve, b.pull, b.pullCurve)) return false;
+            if (!Eq(a.spring, a.springCurve, b.spring, b.springCurve)) return false;
+            if (!Eq(a.stiffness, a.stiffnessCurve, b.stiffness, b.stiffnessCurve)) return false;
+            if (!Eq(a.gravity, a.gravityCurve, b.gravity, b.gravityCurve)) return false;
+            if (!Eq(a.gravityFalloff, a.gravityFalloffCurve, b.gravityFalloff, b.gravityFalloffCurve)) return false;
             if (a.immobileType != b.immobileType) return false;
-            if (!Eq(a.immobile, b.immobile)) return false;
-            if (!Eq(a.immobileCurve, b.immobileCurve)) return false;
+            if (!Eq(a.immobile, a.immobileCurve, b.immobile, b.immobileCurve)) return false;
             // == Limits ==
             if (a.limitType != b.limitType) return false;
-            if (!Eq(a.maxAngleX, b.maxAngleX)) return false;
-            if (!Eq(a.maxAngleXCurve, b.maxAngleXCurve)) return false;
-            if (!Eq(a.maxAngleZ, b.maxAngleZ)) return false;
-            if (!Eq(a.maxAngleZCurve, b.maxAngleZCurve)) return false;
+            if (!Eq(a.maxAngleX, a.maxAngleXCurve, b.maxAngleX, b.maxAngleXCurve)) return false;
+            if (!Eq(a.maxAngleZ, a.maxAngleZCurve, b.maxAngleZ, b.maxAngleZCurve)) return false;
             if (!Eq(a.limitRotation, b.limitRotation)) return false;
             // == Collision ==
-            if (!Eq(a.radius, b.radius)) return false;
-            if (!Eq(a.radiusCurve, b.radiusCurve)) return false;
+            if (!Eq(a.radius, a.radiusCurve, b.radius, b.radiusCurve)) return false;
             if (a.allowCollision != b.allowCollision) return false;
             if (!Eq(a.colliders, b.colliders)) return false;
             // == Grab & Pose ==
             if (a.allowGrabbing != b.allowGrabbing) return false;
             if (a.allowPosing != b.allowPosing) return false;
             if (!Eq(a.grabMovement, b.grabMovement)) return false;
-            if (!Eq(a.maxStretch, b.maxStretch)) return false;
-            if (!Eq(a.maxStretchCurve, b.maxStretchCurve)) return false;
+            if (!Eq(a.maxStretch, a.maxStretchCurve, b.maxStretch, b.maxStretchCurve)) return false;
             // == Options ==
             // Parameter: ignore: must be empty
             // Is Animated: ignore: we can merge them.
