1f963b4a8acd999cdd34f12982e764ff38d804fb
anatawa12 <anatawa12@icloud.com>
2022-12-29 16:55:12 +0900

    chore: Ignore merge source PhysBone from performance calculation

---
+++ Editor/ShouldIgnoreComponentPatch.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using VRC.Dynamics;
+using VRC.SDKBase.Validation.Performance;
+
+namespace Anatawa12.Merger
+{
+    [InitializeOnLoad]
+    public static class ShouldIgnoreComponentPatch
+    {
+        static ShouldIgnoreComponentPatch()
+        {
+            // The API is marked as 'PublicAPI' so I use it.
+            var upstream = AvatarPerformance.ShouldIgnoreComponent;
+            AvatarPerformance.ShouldIgnoreComponent = component =>
+                (upstream?.Invoke(component) ?? false) || ShouldIgnoreComponent(component);
+        }
+
+        private static bool ShouldIgnoreComponent(Component component)
+        {
+            if (component is VRCPhysBoneBase)
+            {
+                // TODO: cache for performance
+                if (Object.FindObjectsOfType<MergePhysBone>()
+                    .SelectMany(x => x.components)
+                    .Any(x => x == component))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
