becbcfc1fdb9da62522eef376be74045b15b25fb
anatawa12 <anatawa12@icloud.com>
2023-04-26 14:29:36 +0900

    chore: remove TODO comment

--- Runtime/DeleteEditorOnlyGameObjects.cs
+++ Runtime/DeleteEditorOnlyGameObjects.cs
@@ -6,6 +6,5 @@ namespace Anatawa12.AvatarOptimizer
     [DisallowMultipleComponent]
     internal class DeleteEditorOnlyGameObjects : AvatarTagComponent
     {
-        // TODO: add option to remove EditorOnly bones of EditorOnly SkinnedMeshRenderer. see #103
     }
 }
