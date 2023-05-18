7db7b4170c795ea162c2dd851c472e89ad4973fa
anatawa12 <anatawa12@icloud.com>
2023-05-18 09:42:38 +0900

    chore: remove unused CL4EELocalized attribute

--- Runtime/MergePhysBone.cs
+++ Runtime/MergePhysBone.cs
@@ -16,7 +16,6 @@ namespace Anatawa12.AvatarOptimizer
         public VRCPhysBoneBase Merged => merged;
 
         [FormerlySerializedAs("mergedComponent")]
-        [CL4EELocalized("MergePhysBone:prop:merged")]
         [SerializeField]
         private VRCPhysBoneBase merged;
 
@@ -26,63 +25,38 @@ namespace Anatawa12.AvatarOptimizer
         [CL4EELocalized("MergePhysBone:prop:makeParent", "MergePhysBone:tooltip:makeParent")]
         public bool makeParent;
 
-        [CL4EELocalized("MergePhysBone:prop:version")]
         public bool version;
         // == Forces ==
         [FormerlySerializedAs("force")]
         [FormerlySerializedAs("forces")]
-        [CL4EELocalized("MergePhysBone:prop:forces")]
         public bool integrationType;
-        [CL4EELocalized("MergePhysBone:prop:pull")]
         public bool pull;
-        [CL4EELocalized("MergePhysBone:prop:spring")]
         public bool spring;
-        [CL4EELocalized("MergePhysBone:prop:stiffness")]
         public bool stiffness;
-        [CL4EELocalized("MergePhysBone:prop:gravity")]
         public bool gravity;
-        [CL4EELocalized("MergePhysBone:prop:gravityFalloff")]
         public bool gravityFalloff;
-        [CL4EELocalized("MergePhysBone:prop:immobileType")]
         public bool immobileType;
-        [CL4EELocalized("MergePhysBone:prop:immobile")]
         public bool immobile;
         // == Limits ==
-        [CL4EELocalized("MergePhysBone:prop:limits")]
         public bool limits;
-        [CL4EELocalized("MergePhysBone:prop:maxAngleX")]
         public bool maxAngleX;
-        [CL4EELocalized("MergePhysBone:prop:maxAngleZ")]
         public bool maxAngleZ;
-        [CL4EELocalized("MergePhysBone:prop:limitRotation")]
         public bool limitRotation;
         // == Collision ==
-        [CL4EELocalized("MergePhysBone:prop:radius")]
         public bool radius;
-        [CL4EELocalized("MergePhysBone:prop:allowCollision")]
         public bool allowCollision;
-        [CL4EELocalized("MergePhysBone:prop:colliders")]
         public CollidersSettings colliders;
         // == Grab & Pose ==
-        [CL4EELocalized("MergePhysBone:prop:allowGrabbing")]
         public bool allowGrabbing;
-        [CL4EELocalized("MergePhysBone:prop:grabMovement")]
         public bool grabMovement;
-        [CL4EELocalized("MergePhysBone:prop:allowPosing")]
         public bool allowPosing;
-        [CL4EELocalized("MergePhysBone:prop:stretchMotion")]
         public bool stretchMotion;
-        [CL4EELocalized("MergePhysBone:prop:maxStretch")]
         public bool maxStretch;
-        [CL4EELocalized("MergePhysBone:prop:maxSquish")]
         public bool maxSquish;
-        [CL4EELocalized("MergePhysBone:prop:snapToHand")]
         public bool snapToHand;
         // == Others ==
         // public bool overrideParameter; Always
-        [CL4EELocalized("MergePhysBone:prop:isAnimated")]
         public bool isAnimated;
-        [CL4EELocalized("MergePhysBone:prop:resetWhenDisabled")]
         public bool resetWhenDisabled;
 
         [Obsolete("legacy v1", true)] [FormerlySerializedAs("component")]
