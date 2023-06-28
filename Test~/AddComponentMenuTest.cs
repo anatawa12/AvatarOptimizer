047429371db2509c24e4931496e9e4b1e559822b
anatawa12 <anatawa12@icloud.com>
2023-06-28 21:24:02 +0900

    chore: add test for AddComponentMenu attribute

---
+++ Test~/AddComponentMenuTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Anatawa12.AvatarOptimizer.Test
+{
+    public class AddComponentMenuTest
+    {
+        [Test]
+        [TestCaseSource(nameof(ComponentTypes))]
+        public void CheckAddComponentMenuIsInAvatarOptimizer(Type type)
+        {
+            var addComponentMenu = type.GetCustomAttribute<AddComponentMenu>();
+            Assert.That(addComponentMenu, Is.Not.Null);
+            Assert.That(addComponentMenu.componentMenu, Does.StartWith("Avatar Optimizer/").Or.Empty);
+        }
+
+        static IEnumerable<Type> ComponentTypes()
+        {
+            return 
+                typeof(AvatarTagComponent).Assembly
+                .GetTypes()
+                .Where(x => x.IsClass && !x.IsAbstract)
+                .Where(x => typeof(MonoBehaviour).IsAssignableFrom(x));
+        }
+    }
+}
