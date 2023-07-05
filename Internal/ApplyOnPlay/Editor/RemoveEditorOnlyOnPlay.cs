d15e2709f09ceec3203a2785ca64971a7d3ee2d1
anatawa12 <anatawa12@icloud.com>
2023-07-05 20:42:46 +0900

    feat(apply-on-play): remove EditorOnly on play

---
+++ Internal/ApplyOnPlay/Editor/RemoveEditorOnlyOnPlay.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Anatawa12.AvatarOptimizer.ApplyOnPlay
+{
+    internal class RemoveEditorOnlyOnPlay : IApplyOnPlayCallback
+    {
+        public int callbackOrder => -1024;
+        public string CallbackName => "Remove Editor Only";
+        public string CallbackId => "com.anatawa12.apply-on-play.remove-editor-only";
+
+        public bool ApplyOnPlay(GameObject avatarGameObject)
+        {
+            foreach (var transform in avatarGameObject.GetComponentsInChildren<Transform>(true))
+                if (transform && transform.CompareTag("EditorOnly"))
+                    Object.DestroyImmediate(transform.gameObject);
+            return true;
+        }
+    }
+}
