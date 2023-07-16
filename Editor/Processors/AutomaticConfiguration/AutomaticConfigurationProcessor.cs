5c82734b3085451b42a9c1206d7c95d623e91349
anatawa12 <anatawa12@icloud.com>
2023-07-16 21:15:50 +0900

    chore: move files

--- Editor/Processors/AutomaticConfiguration/AutomaticConfigurationProcessor.cs
+++ Editor/Processors/AutomaticConfiguration/AutomaticConfigurationProcessor.cs
@@ -1,11 +1,14 @@
 namespace Anatawa12.AvatarOptimizer.Processors
 {
-    internal class AutomaticConfigurationProcessor
+    internal partial class AutomaticConfigurationProcessor
     {
         private AutomaticConfiguration _config;
+        private OptimizerSession _session;
+        
 
         public void Process(OptimizerSession session)
         {
+            _session = session;
             _config = session.GetRootComponent<AutomaticConfiguration>();
             if (!_config) return;
 
