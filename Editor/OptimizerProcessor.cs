196f69da45e881003a8d7e15c44ac2183aa60eb7
anatawa12 <anatawa12@icloud.com>
2023-08-09 19:28:34 +0900

    feat: early processing MakeChildren

--- Editor/OptimizerProcessor.cs
+++ Editor/OptimizerProcessor.cs
@@ -67,6 +67,7 @@ namespace Anatawa12.AvatarOptimizer
         private static void DoProcessObject(OptimizerSession session)
         {
             new Processors.UnusedBonesByReferencesToolEarlyProcessor().Process(session);
+            new Processors.MakeChildrenProcessor(early: true).Process(session);
             session.MarkDirtyAll();
         }
     }
@@ -141,7 +142,7 @@ namespace Anatawa12.AvatarOptimizer
             new Processors.MergePhysBoneProcessor().Process(session);
             new Processors.EditSkinnedMeshComponentProcessor().Process(session);
             new Processors.MergeBoneProcessor().Process(session);
-            new Processors.MakeChildrenProcessor().Process(session);
+            new Processors.MakeChildrenProcessor(early: false).Process(session);
 
             new Processors.ApplyObjectMapping().Apply(session);
 
--- Editor/Processors/MakeChildrenProcessor.cs
+++ Editor/Processors/MakeChildrenProcessor.cs
@@ -5,10 +5,18 @@ namespace Anatawa12.AvatarOptimizer.Processors
 {
     internal class MakeChildrenProcessor
     {
+        private readonly bool _early;
+
+        public MakeChildrenProcessor(bool early)
+        {
+            _early = early;
+        }
+
         public void Process(OptimizerSession session)
         {
             BuildReport.ReportingObjects(session.GetComponents<MakeChildren>(), makeChildren =>
             {
+                if (makeChildren.executeEarly != _early) return;
                 foreach (var makeChildrenChild in makeChildren.children.GetAsSet().Where(x => x))
                 {
                     makeChildrenChild.parent = makeChildren.transform;
