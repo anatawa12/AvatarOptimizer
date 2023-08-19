9fbb5d3279e033b96efbd7cab30ce0e68990e327
anatawa12 <anatawa12@icloud.com>
2023-08-20 00:58:57 +0900

    fix: add missisng files

---
+++ Runtime/InternalAutoFreezeMeaninglessBlendShape.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Anatawa12.AvatarOptimizer
+{
+    // TODO move to somewhere else? e.g. in editor module if possible
+    [AddComponentMenu("")]
+    [DisallowMultipleComponent]
+    internal class InternalAutoFreezeMeaninglessBlendShape : EditSkinnedMeshComponent
+    {
+    }
+}
