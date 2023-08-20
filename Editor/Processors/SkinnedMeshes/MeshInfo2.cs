b4d66194ebbdab5c13bae239bbc1b654e5cad856
anatawa12 <anatawa12@icloud.com>
2023-08-20 20:44:46 +0900

    fix: no error context in Multi Pass Rendering error

--- Editor/Processors/SkinnedMeshes/MeshInfo2.cs
+++ Editor/Processors/SkinnedMeshes/MeshInfo2.cs
@@ -83,7 +83,8 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
 
             // TODO: Remove this error once this is supported
             if (sourceMaterials.Length > SubMeshes.Count)
-                BuildReport.LogFatal("Multi Pass Rendering of one SubMesh is NOT Supported YET.");
+                BuildReport.LogFatal("Multi Pass Rendering of one SubMesh is NOT Supported YET.")
+                    ?.WithContext(renderer);
         }
 
         [Conditional("UNITY_ASSERTIONS")]
