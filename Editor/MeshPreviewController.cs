84fbc36f1df2c2fa18bbe83ab579fe3b5fcc76ac
anatawa12 <anatawa12@icloud.com>
2023-09-18 20:59:14 +0900

    feat: MeshPreviewController

---
+++ Editor/MeshPreviewController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Anatawa12.AvatarOptimizer
+{
+    class MeshPreviewController : ScriptableSingleton<MeshPreviewController>
+    {
+        private static readonly Type[] EditorTypes = { };
+
+        public GameObject targetGameObject;
+        public SkinnedMeshRenderer targetRenderer;
+        public bool previewing;
+
+        private void UpdatePreviewing()
+        {
+            if (!previewing) return;
+
+            bool ShouldStopPreview()
+            {
+                // target GameObject disappears
+                if (targetGameObject == null || targetRenderer == null) return true;
+                // animation mode externally exited
+                if (!AnimationMode.InAnimationMode()) return true;
+                // Showing Inspector changed
+                if (ActiveEditorTracker.sharedTracker.activeEditors[0].target != targetGameObject) return true;
+                // Preview Component Not Found
+                if (EditorTypes.All(t => targetGameObject.GetComponent(t) == null)) return true;
+
+                return false;
+            }
+
+            if (ShouldStopPreview())
+                StopPreview();
+        }
+
+        private bool StartPreview(GameObject expectedGameObject = null)
+        {
+            // Already in AnimationMode of other object
+            if (AnimationMode.InAnimationMode()) return false;
+            // Previewing object
+            if (previewing) return false;
+
+            var gameObject = ActiveEditorTracker.sharedTracker.activeEditors[0].target as GameObject;
+            if (gameObject == null) return false;
+            if (expectedGameObject != null && expectedGameObject != gameObject) return false;
+            var renderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+            if (renderer == null) return false;
+            // Editor Components does not exists
+            if (EditorTypes.All(t => gameObject.GetComponent(t) == null)) return false;
+
+            AnimationMode.StartAnimationMode();
+            targetGameObject = gameObject;
+            targetRenderer = renderer;
+            previewing = true;
+            EditorApplication.update -= UpdatePreviewing;
+            EditorApplication.update += UpdatePreviewing;
+            return true;
+        }
+
+        private void StopPreview()
+        {
+            AnimationMode.StopAnimationMode();
+            targetGameObject = null;
+            targetRenderer = null;
+            previewing = false;
+            EditorApplication.update -= UpdatePreviewing;
+        }
+    }
+}
