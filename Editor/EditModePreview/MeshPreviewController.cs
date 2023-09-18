0f1b7ee58aee7828f773a0e7ba49e352994ff4a6
anatawa12 <anatawa12@icloud.com>
2023-09-19 00:38:26 +0900

    chore: move MeshPreviewController

--- Editor/EditModePreview/MeshPreviewController.cs
+++ Editor/EditModePreview/MeshPreviewController.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-namespace Anatawa12.AvatarOptimizer
+namespace Anatawa12.AvatarOptimizer.EditModePreview
 {
     class MeshPreviewController : ScriptableSingleton<MeshPreviewController>
     {
