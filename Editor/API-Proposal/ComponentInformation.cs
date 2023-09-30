a01cd3b3ff35fe95bf6380833d658843881cb60f
anatawa12 <anatawa12@icloud.com>
2023-09-30 15:44:01 +0900

    chore: add ComponentInformation<T>

---
+++ Editor/API-Proposal/ComponentInformation.cs
@@ -0,0 +1,53 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Anatawa12.AvatarOptimizer.API
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    [MeansImplicitUse]
+    [BaseTypeRequired(typeof(ComponentInformation<>))]
+    public sealed class ComponentInformationAttribute : Attribute
+    {
+        public Type TargetType { get; }
+
+        public ComponentInformationAttribute(Type targetType)
+        {
+            TargetType = targetType;
+        }
+    }
+
+    public interface IComponentDependencyCollector
+    {
+        void MarkEntrypoint();
+
+        IComponentDependencyInfo AddDependency(Component dependant, Component dependency);
+        IComponentDependencyInfo AddDependency(Component dependency);
+    }
+
+    /// <summary>
+    /// This interface will never be stable for implement. This interface is stable for calling methods.
+    /// </summary>
+    public interface IComponentDependencyInfo
+    {
+        IComponentDependencyInfo EvenIfDependantDisabled();
+        IComponentDependencyInfo OnlyIfTargetCanBeEnable();
+
+        IComponentDependencyInfo AsBone();
+    }
+
+    internal interface IComponentInformation
+    {
+        void CollectDependency(Component component, IComponentDependencyCollector collector);
+    }
+
+    [MeansImplicitUse]
+    public abstract class ComponentInformation<T> : IComponentInformation
+        where T : Component
+    {
+        void IComponentInformation.CollectDependency(Component component, IComponentDependencyCollector collector) =>
+            CollectDependency((T)component, collector);
+
+        protected abstract void CollectDependency(T component, IComponentDependencyCollector collector);
+    }
+}
