263ff91aa1aba65ec2b00786ffec13e167ce6c64
anatawa12 <anatawa12@icloud.com>
2023-10-14 20:25:40 +0900

    chore: show meshes without normals on the error

--- Editor/Processors/SkinnedMeshes/MergeSkinnedMeshProcessor.cs
+++ Editor/Processors/SkinnedMeshes/MergeSkinnedMeshProcessor.cs
@@ -27,9 +27,11 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
 
         public override void Process(OptimizerSession session, MeshInfo2 target)
         {
+            var skinnedMeshRenderers = SkinnedMeshRenderers.ToList();
+            var staticMeshRenderers = StaticMeshRenderers.ToList();
             {
                 HashSet<string> thisPreserve = null;
-                foreach (var skinnedRenderer in SkinnedMeshRenderers)
+                foreach (var skinnedRenderer in skinnedMeshRenderers)
                 {
                     if (!session.PreserveBlendShapes.TryGetValue(skinnedRenderer, out var preserve)) continue;
 
@@ -39,8 +41,8 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
                 }
             }
 
-            var meshInfos = SkinnedMeshRenderers.Select(session.MeshInfo2Holder.GetMeshInfoFor)
-                .Concat(StaticMeshRenderers.Select(session.MeshInfo2Holder.GetMeshInfoFor))
+            var meshInfos = skinnedMeshRenderers.Select(session.MeshInfo2Holder.GetMeshInfoFor)
+                .Concat(staticMeshRenderers.Select(session.MeshInfo2Holder.GetMeshInfoFor))
                 .ToArray();
             var sourceMaterials = meshInfos.Select(x => x.SubMeshes.Select(y => y.SharedMaterial).ToArray()).ToArray();
 
@@ -54,7 +56,24 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
 
             if (hasNormal == 3)
             {
-                BuildReport.LogFatal("MergeSkinnedMesh:error:mix-normal-existence")?.WithContext(Component);
+                // collect (skinned) mesh renderers who doesn't have normal
+                // to show the list on the error reporting
+                var meshes = new Renderer[meshInfos.Length];
+                for (var i = 0; i < skinnedMeshRenderers.Count; i++)
+                    meshes[i] = skinnedMeshRenderers[i];
+                for (var i = 0; i < staticMeshRenderers.Count; i++)
+                    meshes[i + skinnedMeshRenderers.Count] = staticMeshRenderers[i];
+
+                var meshesWithoutNormals = new List<Renderer>();
+                for (var i = 0; i < meshInfos.Length; i++)
+                {
+                    var meshInfo2 = meshInfos[i];
+                    if (meshInfo2.Vertices.Count != 0 && !meshInfo2.HasNormals)
+                        meshesWithoutNormals.Add(meshes[i]);
+                }
+                // ReSharper disable once CoVariantArrayConversion
+                BuildReport.LogFatal("MergeSkinnedMesh:error:mix-normal-existence")
+                    ?.WithContext((object[])meshesWithoutNormals.ToArray());
             }
 
             var (subMeshIndexMap, materials) = CreateMergedMaterialsAndSubMeshIndexMapping(sourceMaterials);
