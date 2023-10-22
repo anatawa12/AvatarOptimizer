ddcd71fe5e5fbd71375b7c54433ec147fc4aa6b3
anatawa12 <anatawa12@icloud.com>
2023-10-22 15:00:56 +0900

    feat: debug GC system again

--- Editor/Processors/TraceAndOptimize/FindUnusedObjectsProcessor.cs
+++ Editor/Processors/TraceAndOptimize/FindUnusedObjectsProcessor.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using nadena.dev.ndmf;
+using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -22,10 +25,7 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
             else
             {
                 var processor = new FindUnusedObjectsProcessor(context, state);
-                //if (state.GCDebug)
-                //    processor.CollectDataForGc();
-                //else
-                    processor.ProcessNew();
+                processor.ProcessNew();
             }
         }
     }
@@ -81,6 +81,7 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
         private readonly HashSet<GameObject> _exclusions;
         private readonly bool _preserveEndBone;
         private readonly bool _noConfigureMergeBone;
+        private readonly bool _gcDebug;
 
         public FindUnusedObjectsProcessor(BuildContext context, TraceAndOptimizeState state)
         {
@@ -89,6 +90,7 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
             _modifications = state.Modifications;
             _preserveEndBone = state.PreserveEndBone;
             _noConfigureMergeBone = state.NoConfigureMergeBone;
+            _gcDebug = state.GCDebug;
             _exclusions = state.Exclusions;
         }
 
@@ -96,6 +98,11 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
         {
             var componentInfos = new GCComponentInfoHolder(_modifications, _context.AvatarRootObject);
             Mark(componentInfos);
+            if (_gcDebug)
+            {
+                GCDebug(componentInfos);
+                return;
+            }
             Sweep(componentInfos);
             if (!_noConfigureMergeBone)
                 MergeBone(componentInfos);
@@ -280,6 +287,132 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
                     case false:
                         // This GameObject and their children will never be active
                         break;
+                }
+            }
+        }
+        
+        private void GCDebug(GCComponentInfoHolder componentInfos)
+        {
+            foreach (var componentInfo in componentInfos.AllInformation)
+            {
+                var gcDebugInfo = componentInfo.Component.gameObject.AddComponent<GCDebugInfo>();
+                gcDebugInfo.component = componentInfo.Component;
+                gcDebugInfo.activeness = GCDebugInfo.ActivenessFromBool(componentInfo.Activeness);
+                gcDebugInfo.isEntryPoint = componentInfo.EntrypointComponent;
+                gcDebugInfo.entryPoints = componentInfo.DependantEntrypoint.Select(GCDebugInfo.ComponentTypePair.From).ToArray();
+                gcDebugInfo.dependencies = componentInfo.Dependencies.Select(GCDebugInfo.ComponentTypePair.From).ToArray();
+            }
+
+            throw new System.NotImplementedException();
+        }
+
+        class GCDebugRoot : MonoBehaviour { }
+
+        [CustomEditor(typeof(GCDebugRoot))]
+        class GCDebugRootEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                if (GUILayout.Button("Copy All Data"))
+                {
+                    GUIUtility.systemCopyBuffer = CreateData();
+                }
+
+                if (GUILayout.Button("Save All Data"))
+                {
+                    var path = EditorUtility.SaveFilePanel("DebugGCData", "", "data.txt", "txt");
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        System.IO.File.WriteAllText(path, CreateData());
+                    }
+                }
+
+                string CreateData()
+                {
+                    var root = ((Component)target).gameObject;
+                    var collect = new StringBuilder();
+                    foreach (var gcData in root.GetComponentsInChildren<GCDebugInfo>(true))
+                    {
+                        collect.Append(RuntimeUtil.RelativePath(root, gcData.gameObject))
+                            .Append("(").Append(gcData.component.GetType().Name).Append("):\n");
+                        collect.Append("  IsEntryPoint: ").Append(gcData.isEntryPoint).Append('\n');
+                        collect.Append("  ActiveNess: ").Append(gcData.activeness).Append('\n');
+                        collect.Append("  Dependencies:\n");
+                        foreach (var line in PairsToStrings(root, gcData.dependencies))
+                            collect.Append("    ").Append(line).Append("\n");
+                        collect.Append("  EntryPoints:\n");
+                        foreach (var line in PairsToStrings(root, gcData.entryPoints))
+                            collect.Append("    ").Append(line).Append("\n");
+                        collect.Append("\n");
+                    }
+
+                    return collect.ToString();
+                }
+
+                IEnumerable<string> PairsToStrings(GameObject root, GCDebugInfo.ComponentTypePair[] pairs) =>
+                    from pair in pairs
+                    where pair.component != null
+                    let path = RuntimeUtil.RelativePath(root, pair.component.gameObject)
+                    let type = pair.component.GetType().Name
+                    let processing = $"{path}({type}): {pair.type}"
+                    orderby processing
+                    select processing;
+            }
+        }
+
+        class GCDebugInfo : MonoBehaviour
+        {
+            public Component component;
+            public Activeness activeness;
+            public bool isEntryPoint;
+            public ComponentTypePair[] dependencies;
+            public ComponentTypePair[] entryPoints;
+
+            public static Activeness ActivenessFromBool(bool? activeness)
+            {
+                if (activeness == true)
+                    return Activeness.Active;
+                if (activeness == false)
+                    return Activeness.Inactive;
+                return Activeness.Variable;
+            }
+
+            public enum Activeness
+            {
+                Active,
+                Inactive,
+                Variable
+            }
+
+            [Serializable]
+            public struct ComponentTypePair
+            {
+                public Component component;
+                public GCComponentInfo.DependencyType type;
+
+                public static ComponentTypePair From(KeyValuePair<Component, GCComponentInfo.DependencyType> arg)
+                {
+                    return new ComponentTypePair
+                    {
+                        component = arg.Key,
+                        type = arg.Value,
+                    };
+                }
+            }
+
+            [CustomPropertyDrawer(typeof(ComponentTypePair))]
+            private class ComponentTypePairEditor : PropertyDrawer
+            {
+                public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+                {
+                    var indent = EditorGUI.indentLevel * 15f;
+                    const float spacing = 2.0f;
+                    var labelPosition = new Rect(position.x + indent, position.y, EditorGUIUtility.labelWidth - indent, position.height);
+                    var rect = new Rect(position.x + EditorGUIUtility.labelWidth + spacing, position.y, 
+                        position.width - EditorGUIUtility.labelWidth - spacing, position.height);
+
+                    EditorGUI.ObjectField(labelPosition, property.FindPropertyRelative("component"));
+                    GUI.Label(rect, ((GCComponentInfo.DependencyType)property.FindPropertyRelative("type").intValue).ToString());
                 }
             }
         }
