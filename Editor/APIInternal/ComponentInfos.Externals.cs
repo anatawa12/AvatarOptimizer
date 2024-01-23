16a759d4888183e26f677aa2a88c3c2fdf66a6bb
anatawa12 <anatawa12@icloud.com>
2024-01-23 23:11:02 +0900

    chore: migrate ComponentInfos.Externals to AssetDescriptions and remove description for quest tools

--- Editor/APIInternal/ComponentInfos.Externals.cs
+++ Editor/APIInternal/ComponentInfos.Externals.cs
@@ -1,6 +1,3 @@
-using Anatawa12.AvatarOptimizer.API;
-using UnityEngine;
-
 namespace Anatawa12.AvatarOptimizer.APIInternal.Externals
 {
     /// <summary>
@@ -12,31 +9,4 @@ namespace Anatawa12.AvatarOptimizer.APIInternal.Externals
     internal interface IExternalMarker
     {
     }
-
-    #region Satania's KiseteneEx Components
-
-    [ComponentInformationWithGUID("e78466b6bcd24e5409dca557eb81d45b", 11500000)] // KiseteneComponent
-    [ComponentInformationWithGUID("7f9c3fe1cfb9d1843a9dc7da26352ce2", 11500000)] // FlyAvatarSetupTool
-    [ComponentInformationWithGUID("95f6e1368d803614f8a351322ab09bac", 11500000)] // BlendShapeOverrider
-    internal class SataniaKiseteneExComponents : ComponentInformation<Component>, IExternalMarker
-    {
-        protected override void CollectDependency(Component component, ComponentDependencyCollector collector)
-        {
-        }
-    }
-
-    #endregion
-
-    
-    #region VRCQuestTools
-
-    [ComponentInformationWithGUID("f055e14e1beba894ea68aedffde8ada6", 11500000)] // VertexColorRemover
-    internal class VRCQuestToolsComponents : ComponentInformation<Component>, IExternalMarker
-    {
-        protected override void CollectDependency(Component component, ComponentDependencyCollector collector)
-        {
-        }
-    }
-
-    #endregion
 }
