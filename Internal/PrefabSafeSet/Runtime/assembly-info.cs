0b5f9c66fcc4256e75e51713e1ec746ab8d58f0a
anatawa12 <anatawa12@icloud.com>
2024-01-30 03:35:23 +0900

    chore: remove assembly-level CL4EE configuration

--- Internal/PrefabSafeSet/Runtime/assembly-info.cs
+++ Internal/PrefabSafeSet/Runtime/assembly-info.cs
@@ -2,4 +2,3 @@ using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("com.anatawa12.avatar-optimizer.internal.prefab-safe-set.editor")]
 [assembly:InternalsVisibleTo("com.anatawa12.avatar-optimizer.test")]
-[assembly:CustomLocalization4EditorExtension.RedirectCL4EEInstance("com.anatawa12.avatar-optimizer.editor")]
