f7d415c054ecc8ec3da73eedf36f9a378353f369
anatawa12 <anatawa12@icloud.com>
2024-03-21 21:24:04 +0900

    fix: InitializeAnimatorOptimizer is not disabled in 2021.2 or older

--- Editor/Processors/InitializeAnimatorOptimizer.cs
+++ Editor/Processors/InitializeAnimatorOptimizer.cs
@@ -1,3 +1,5 @@
+#if UNITY_2021_3_OR_NEWER
+
 using System;
 using System.Collections.Generic;
 using Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes;
@@ -176,3 +178,5 @@ namespace Anatawa12.AvatarOptimizer.Processors.AnimatorOptimizer
         }
     }
 }
+
+#endif
