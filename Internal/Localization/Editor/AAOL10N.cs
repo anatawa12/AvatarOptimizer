1b365f470168fd859622aa3d57ada35ca75dca93
flower_elf <47104993+flower-elf@users.noreply.github.com>
2024-05-06 19:47:22 +0800

    Add Simplified Chinese translation

--- Internal/Localization/Editor/AAOL10N.cs
+++ Internal/Localization/Editor/AAOL10N.cs
@@ -19,6 +19,7 @@ namespace Anatawa12.AvatarOptimizer
             {
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "en-us.po"),
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "ja-jp.po"),
+                AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "zh-cn.po"),
             };
         });
 
