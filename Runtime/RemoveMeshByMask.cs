6a31bb3bc3a72a5476d8f376a051e659306e2d2b
Reina_Sakiria <sasiki.64892@gmail.com>
2024-07-29 21:13:06 +0900

    feat(api): Make RemoveMeshByMask public API

--- Runtime/RemoveMeshByMask.cs
+++ Runtime/RemoveMeshByMask.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Anatawa12.AvatarOptimizer
@@ -7,29 +9,61 @@ namespace Anatawa12.AvatarOptimizer
     [RequireComponent(typeof(SkinnedMeshRenderer))]
     [DisallowMultipleComponent]
     [HelpURL("https://vpm.anatawa12.com/avatar-optimizer/ja/docs/reference/remove-mesh-by-mask/")]
-    internal sealed class RemoveMeshByMask : EditSkinnedMeshComponent, INoSourceEditSkinnedMeshComponent
+    [PublicAPI]
+    public sealed class RemoveMeshByMask : EditSkinnedMeshComponent, INoSourceEditSkinnedMeshComponent
     {
         [SerializeField]
         internal MaterialSlot[] materials = Array.Empty<MaterialSlot>();
 
         [Serializable]
-        internal struct MaterialSlot
+        [PublicAPI]
+        public struct MaterialSlot
         {
             [SerializeField]
             [ToggleLeft]
-            public bool enabled;
+            internal bool enabled;
             [SerializeField]
             [AAOLocalized("RemoveMeshByMask:prop:mask")]
-            public Texture2D mask;
+            internal Texture2D mask;
             [SerializeField]
             [AAOLocalized("RemoveMeshByMask:prop:mode")]
-            public RemoveMode mode;
+            internal RemoveMode mode;
+
+            [PublicAPI]
+            public bool Enabled
+            {
+                get => enabled;
+                set => enabled = value;
+            }
+
+            [PublicAPI]
+            public Texture2D Mask
+            {
+                get => mask;
+                set => mask = value;
+            }
+
+            [PublicAPI]
+            public RemoveMode Mode
+            {
+                get => mode;
+                set => mode = value;
+            }
+
         }
 
-        internal enum RemoveMode
+        [PublicAPI]
+        public enum RemoveMode
         {
             RemoveBlack,
             RemoveWhite,
+        }
+
+        [PublicAPI]
+        public MaterialSlot[] Materials
+        {
+            get => materials.ToArray();
+            set => materials = value.ToArray();
         }
     }
 }
