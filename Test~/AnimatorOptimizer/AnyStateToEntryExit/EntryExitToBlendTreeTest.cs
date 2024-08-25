66a21b051fd75c72ccc73272a3fb70eafd0ba484
anatawa12 <anatawa12@icloud.com>
2024-08-26 00:37:45 +0900

    test: add test with Gesture layer of HAOLAN

--- Test~/AnimatorOptimizer/AnyStateToEntryExit/EntryExitToBlendTreeTest.cs
+++ Test~/AnimatorOptimizer/AnyStateToEntryExit/EntryExitToBlendTreeTest.cs
@@ -8,6 +8,7 @@ namespace Anatawa12.AvatarOptimizer.Test.AnimatorOptimizer
     {
         private AnimatorOptimizerState _state = new AnimatorOptimizerState();
 
+        // This basically animates BlendShapes, which we can determine if they are no-op or not
         [Test]
         public void HaolanGestureFace()
         {
@@ -15,6 +16,17 @@ namespace Anatawa12.AvatarOptimizer.Test.AnimatorOptimizer
             controller.name = "HAOLAN-GestureFace.converted";
             AnyStateToEntryExit.Execute(_state, new AOAnimatorController(controller));
             var except = LoadAnimatorController("HAOLAN-GestureFace.converted");
+            RecursiveCheckEquals(except, controller);
+        }
+        
+        // This animates Humanoid bones (Animator parameters), which we cannot determine if they are no-op or not 
+        [Test]
+        public void HaolanGestureHand()
+        {
+            var controller = LoadCloneAnimatorController("HAOLAN-GestureHand");
+            controller.name = "HAOLAN-GestureHand.converted";
+            AnyStateToEntryExit.Execute(_state, new AOAnimatorController(controller));
+            var except = LoadAnimatorController("HAOLAN-GestureHand.converted");
             RecursiveCheckEquals(except, controller);
         }
     }
