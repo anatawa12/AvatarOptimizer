5c4c7eec89fc84a28e7aefb0cd8fcc5298acac13
anatawa12 <anatawa12@icloud.com>
2024-08-26 18:46:06 +0900

    test: add tests with RIS

--- Test~/AnimatorOptimizer/AnyStateToEntryExit/EntryExitToBlendTreeTest.cs
+++ Test~/AnimatorOptimizer/AnyStateToEntryExit/EntryExitToBlendTreeTest.cs
@@ -29,5 +29,30 @@ namespace Anatawa12.AvatarOptimizer.Test.AnimatorOptimizer
             var except = LoadAnimatorController("HAOLAN-GestureHand.converted");
             RecursiveCheckEquals(except, controller);
         }
+
+        // RIS Initialized state will have no motion so no-op check fails.
+        // therefore, Initialize layer will not be converted  
+        [Test]
+        public void RISNoRoot()
+        {
+            var controller = LoadCloneAnimatorController("RIS-Test");
+            controller.name = "RIS-Test.converted.noroot";
+            AnyStateToEntryExit.Execute(_state, new AOAnimatorController(controller));
+            var except = LoadAnimatorController("RIS-Test.converted.noroot");
+            RecursiveCheckEquals(except, controller);
+        }
+
+        // RIS Initialized state will have no motion so no-op check fails.
+        // therefore, Initialize layer will not be converted  
+        [Test]
+        public void RISWithRoot()
+        {
+            var controller = LoadCloneAnimatorController("RIS-Test");
+            var root = LoadPrefab("RIS-Test");
+            controller.name = "RIS-Test.converted.withroot";
+            AnyStateToEntryExit.Execute(_state, new AOAnimatorController(controller, root));
+            var except = LoadAnimatorController("RIS-Test.converted.withroot");
+            RecursiveCheckEquals(except, controller);
+        }
     }
 }
