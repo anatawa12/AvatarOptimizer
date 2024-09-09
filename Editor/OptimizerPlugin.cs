2747310477d3756cabeec403b206ea28d56131ac
anatawa12 <anatawa12@icloud.com>
2024-09-09 12:48:09 +0900

    chore: clone material before processing / parsing material

--- Editor/OptimizerPlugin.cs
+++ Editor/OptimizerPlugin.cs
@@ -54,6 +54,7 @@ namespace Anatawa12.AvatarOptimizer.ndmf
                             })
                         .Then.Run("Validation", (ctx) => ComponentValidation.ValidateAll(ctx.AvatarRootObject))
                         .Then.Run(Processors.TraceAndOptimizes.LoadTraceAndOptimizeConfiguration.Instance)
+                        .Then.Run(Processors.DupliacteAssets.Instance)
                         .Then.Run(Processors.ParseAnimator.Instance)
                         .Then.Run(Processors.TraceAndOptimizes.AddRemoveEmptySubMesh.Instance)
                         .Then.Run(Processors.TraceAndOptimizes.AutoFreezeBlendShape.Instance)
