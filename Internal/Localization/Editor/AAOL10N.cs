d574984f66dd879715a51b81f0eae7877e77c9c0
anatawa12 <anatawa12@icloud.com>
2024-10-14 17:10:14 +0900

    chore: rename zh-cn to zh-hans

--- Internal/Localization/Editor/AAOL10N.cs
+++ Internal/Localization/Editor/AAOL10N.cs
@@ -18,7 +18,7 @@ namespace Anatawa12.AvatarOptimizer
             {
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "en-us.po"),
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "ja-jp.po"),
-                AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "zh-cn.po"),
+                AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "zh-hans.po"),
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(localizationFolder + "zh-hant.po"),
             };
         });
