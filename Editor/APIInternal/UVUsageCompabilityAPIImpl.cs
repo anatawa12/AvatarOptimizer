abd01a00c4de81e2f60198bacbfb257039b227b1
anatawa12 <anatawa12@icloud.com>
2024-10-16 10:50:21 +0900

    feat: UVUsageCompabilityAPI for TTT AtlasTexture compatibility

---
+++ Editor/APIInternal/UVUsageCompabilityAPIImpl.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Anatawa12.AvatarOptimizer.API;
+using UnityEditor;
+using UnityEngine;
+
+namespace Anatawa12.AvatarOptimizer.APIInternal;
+
+public class UVUsageCompabilityAPIImpl : UVUsageCompabilityAPI.IUVUsageCompabilityAPIImpl
+{
+    [InitializeOnLoadMethod]
+    private static void Register()
+    {
+        UVUsageCompabilityAPI.Impl = new UVUsageCompabilityAPIImpl();
+    }
+
+    public class Evacuate : EditSkinnedMeshComponent
+    {
+        public List<Evacuation> evacuations = new List<Evacuation>();
+        
+        [Serializable]
+        public struct Evacuation
+        {
+            public int originalChannel;
+            public int savedChannel;
+        }
+    }
+
+    public class RevertEvacuate : EditSkinnedMeshComponent
+    {
+        public Evacuate evacuate = null!; // initialized on add
+    }
+
+    private BitArray GetUsedChannels(SkinnedMeshRenderer renderer)
+    {
+        var usedChannels = new BitArray(8);
+
+        var removeMeshByMask = renderer.GetComponent<RemoveMeshByMask>() as RemoveMeshByMask;
+        var removeMeshByUVTile = renderer.GetComponent<RemoveMeshByUVTile>() as RemoveMeshByUVTile;
+        var evacuate = renderer.GetComponent<Evacuate>() as Evacuate;
+
+        if (removeMeshByMask != null)
+        {
+            usedChannels[0] = true;
+        }
+
+        if (removeMeshByUVTile != null)
+        {
+            foreach (var materialSlot in removeMeshByUVTile.materials)
+            {
+                if (materialSlot.RemoveAnyTile)
+                {
+                    usedChannels[(int)materialSlot.uvChannel] = true;
+                }
+            }
+        }
+
+        if (evacuate != null)
+        {
+            foreach (var evacuateEvacuation in evacuate.evacuations)
+            {
+                (usedChannels[evacuateEvacuation.savedChannel], usedChannels[evacuateEvacuation.originalChannel])
+                    = (usedChannels[evacuateEvacuation.originalChannel], usedChannels[evacuateEvacuation.savedChannel]);
+            }
+        }
+
+        return usedChannels;
+    }
+
+    public bool IsTexCoordUsed(SkinnedMeshRenderer renderer, int channel) => GetUsedChannels(renderer)[channel];
+
+    public void RegisterTexCoordEvacuation(SkinnedMeshRenderer renderer, int originalChannel, int savedChannel)
+    {
+        var channels = GetUsedChannels(renderer);
+        if (channels[savedChannel]) throw new InvalidOperationException("savedChannel is used by AAO");
+        var evacuate = renderer.GetComponent<Evacuate>();
+        if (evacuate == null)
+        {
+            evacuate = renderer.gameObject.AddComponent<Evacuate>();
+            renderer.gameObject.AddComponent<RevertEvacuate>().evacuate = evacuate;
+        }
+        evacuate.evacuations.Add(new Evacuate.Evacuation
+        {
+            originalChannel = originalChannel,
+            savedChannel = savedChannel,
+        });
+    }
+}
---
+++ Editor/Processors/SkinnedMeshes/EvacuateProcessors.cs
@@ -0,0 +1,67 @@
+using System.Threading.Tasks;
+using Anatawa12.AvatarOptimizer.APIInternal;
+using nadena.dev.ndmf;
+
+namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes;
+
+internal class EvacuateProcessor : EditSkinnedMeshProcessor<UVUsageCompabilityAPIImpl.Evacuate>
+{
+    public EvacuateProcessor(UVUsageCompabilityAPIImpl.Evacuate component) : base(component)
+    {
+    }
+
+    public override EditSkinnedMeshProcessorOrder ProcessOrder => EditSkinnedMeshProcessorOrder.Evacuate;
+
+    public override void Process(BuildContext context, MeshInfo2 target)
+    {
+        var swaps = Component.evacuations;
+
+        Parallel.ForEach(target.Vertices, vertex =>
+        {
+            foreach (var swap in swaps)
+            {
+                var original = vertex.GetTexCoord(swap.originalChannel);
+                var saved = vertex.GetTexCoord(swap.savedChannel);
+
+                vertex.SetTexCoord(swap.originalChannel, saved);
+                vertex.SetTexCoord(swap.savedChannel, original);
+            }
+        });
+    }
+
+    public override IMeshInfoComputer GetComputer(IMeshInfoComputer upstream) => upstream;
+}
+
+internal class RevertEvacuateProcessor : EditSkinnedMeshProcessor<UVUsageCompabilityAPIImpl.RevertEvacuate>
+{
+    public RevertEvacuateProcessor(UVUsageCompabilityAPIImpl.RevertEvacuate component) : base(component)
+    {
+    }
+
+    public override EditSkinnedMeshProcessorOrder ProcessOrder => EditSkinnedMeshProcessorOrder.ReverseEvacuate;
+
+    public override void Process(BuildContext context, MeshInfo2 target)
+    {
+        var swaps = Component.evacuate.evacuations;
+
+        Parallel.ForEach(target.Vertices, vertex =>
+        {
+            for (var index = swaps.Count - 1; index >= 0; index--)
+            {
+                var swap = swaps[index];
+                var saved = vertex.GetTexCoord(swap.savedChannel);
+
+                vertex.SetTexCoord(swap.originalChannel, saved);
+            }
+        });
+        
+        // remove saved UV
+        for (var index = swaps.Count - 1; index >= 0; index--)
+        {
+            var swap = swaps[index];
+            target.SetTexCoordStatus(swap.savedChannel, TexCoordStatus.NotDefined);
+        }
+    }
+
+    public override IMeshInfoComputer GetComputer(IMeshInfoComputer upstream) => upstream;
+}
