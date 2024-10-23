ebde19ee09e5ca52f2d80b36d2f2f79e36514d25
tliks <aoft725@gmail.com>
2024-10-23 21:02:21 +0900

    fix: Match the name with the inspector

--- Editor/NewGameObjectAndAddComponent.cs
+++ Editor/NewGameObjectAndAddComponent.cs
@@ -6,13 +6,15 @@ namespace Anatawa12.AvatarOptimizer
     internal class NewGameObjectAndAddComponent
     {
         private const string BASE_PATH = "GameObject/Avatar Optimizer/";
+        private const string MSM = "Merge Skinned Mesh";
+        private const string MPB = "Merge PhysBone";
 
-        private static void CreateAndAddComponent<T>() where T : MonoBehaviour
+        private static void CreateAndAddComponent<T>(string componentName) where T : MonoBehaviour
         {
             var selectedObject = Selection.activeGameObject;
             if (selectedObject == null) return;
 
-            var newGameObject = new GameObject(typeof(T).Name);
+            var newGameObject = new GameObject(componentName);
             var transform = newGameObject.transform;
             transform.SetParent(selectedObject.transform, false);
             transform.localPosition = Vector3.zero;
@@ -21,21 +23,23 @@ namespace Anatawa12.AvatarOptimizer
             
             newGameObject.AddComponent<T>();
 
-            Undo.RegisterCreatedObjectUndo(newGameObject, $"Create {typeof(T).Name}");
+            Undo.RegisterCreatedObjectUndo(newGameObject, $"Create {componentName}");
             Selection.activeGameObject = newGameObject;
             EditorGUIUtility.PingObject(newGameObject);
         }
 
-        [MenuItem(BASE_PATH + nameof(MergeSkinnedMesh), true)]
+
+        [MenuItem(BASE_PATH + MSM, true)]
         private static bool ValidateCreateMergeSkinnedMesh() => Selection.activeGameObject != null;
 
-        [MenuItem(BASE_PATH + nameof(MergeSkinnedMesh))]
-        private static void CreateMergeSkinnedMesh() => CreateAndAddComponent<MergeSkinnedMesh>();
+        [MenuItem(BASE_PATH + MSM)]
+        private static void CreateMergeSkinnedMesh() => CreateAndAddComponent<MergeSkinnedMesh>(MSM);
 
-        [MenuItem(BASE_PATH + nameof(MergePhysBone), true)]
+
+        [MenuItem(BASE_PATH + MPB, true)]
         private static bool ValidateCreateMergePhysBone() => Selection.activeGameObject != null;
 
-        [MenuItem(BASE_PATH + nameof(MergePhysBone))]
-        private static void CreateMergePhysBone() => CreateAndAddComponent<MergePhysBone>();
+        [MenuItem(BASE_PATH + MPB)]
+        private static void CreateMergePhysBone() => CreateAndAddComponent<MergePhysBone>(MPB);
     }
 }
