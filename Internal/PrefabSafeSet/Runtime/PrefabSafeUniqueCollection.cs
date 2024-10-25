8f5c8342ec2684b4c7c3483d5322f9a6dcb27748
anatawa12 <anatawa12@icloud.com>
2024-10-25 21:47:44 +0900

    fix: PrefabSafeUniqueCollection does not consider unity fake null

--- Internal/PrefabSafeSet/Runtime/PrefabSafeUniqueCollection.cs
+++ Internal/PrefabSafeSet/Runtime/PrefabSafeUniqueCollection.cs
@@ -374,9 +374,9 @@ namespace Anatawa12.AvatarOptimizer.PrefabSafeUniqueCollection
 
             foreach (var value in initialize)
             {
-                if (value == null) continue;
+                if (value.IsNull()) continue;
                 var key = manipulator.GetKey(value);
-                if (key == null) continue;
+                if (key.IsNull()) continue;
                 _index.Add(key, _list.AddLast(value));
             }
         }
@@ -388,9 +388,9 @@ namespace Anatawa12.AvatarOptimizer.PrefabSafeUniqueCollection
 
         public void Add(TAdditionValue? value)
         {
-            if (value == null) return;
+            if (value.IsNull()) return;
             var key = _manipulator.GetKey(value);
-            if (key == null) return;
+            if (key.IsNull()) return;
             if (_index.TryGetValue(key, out var node))
                 node.Value = value;
             else
