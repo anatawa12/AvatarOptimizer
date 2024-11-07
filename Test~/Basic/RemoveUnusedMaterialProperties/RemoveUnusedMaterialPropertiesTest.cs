0ed5f903939d57ff19fb11dd91a743dd5924a01c
anatawa12 <anatawa12@icloud.com>
2024-11-07 15:46:20 +0900

    test: add check if unused properties have been deleted

--- Test~/Basic/RemoveUnusedMaterialProperties/RemoveUnusedMaterialPropertiesTest.cs
+++ Test~/Basic/RemoveUnusedMaterialProperties/RemoveUnusedMaterialPropertiesTest.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 
 namespace Anatawa12.AvatarOptimizer.Test;
@@ -20,12 +22,6 @@ public class RemoveUnusedMaterialPropertiesTest
         var expectedColor = new Color(1, 0, 0, 1);
         var expectedColor2nd = new Color(0, 1, 0, 1);
 
-        // check before removing
-        //Assert.That(material.GetTexture("_MainTex"), Is.EqualTo(expectedMainTex));
-        //Assert.That(material.GetTexture("_MainTex2nd"), Is.EqualTo(expectedMainTex2nd));
-        //Assert.That(material.GetColor("_Color"), Is.EqualTo(expectedColor));
-        //Assert.That(material.GetColor("_Color2nd"), Is.EqualTo(expectedColor2nd));
-
         // remove unused properties
         RemoveUnusedMaterialProperties.RemoveUnusedProperties(material);
 
@@ -34,5 +30,29 @@ public class RemoveUnusedMaterialPropertiesTest
         Assert.That(material.GetTexture("_MainTex2nd"), Is.EqualTo(expectedMainTex2nd));
         Assert.That(material.GetColor("_Color"), Is.EqualTo(expectedColor));
         Assert.That(material.GetColor("_Color2nd"), Is.EqualTo(expectedColor2nd));
+
+        Assert.That(GetMaterialPropertyNames(material),
+            Is.EquivalentTo(new[] { "_MainTex", "_MainTex2nd", "_Color", "_Color2nd" }));
+    }
+
+    private IEnumerable<string> GetMaterialPropertyNames(Material material)
+    {
+        using var so = new SerializedObject(material);
+        var properties = new[]
+        {
+            so.FindProperty("m_SavedProperties.m_TexEnvs"),
+            so.FindProperty("m_SavedProperties.m_Floats"),
+            so.FindProperty("m_SavedProperties.m_Colors"),
+        };
+
+        foreach (var property in properties)
+        {
+            if (property == null) continue;
+            for (var i = 0; i < property.arraySize; i++)
+            {
+                var name = property.GetArrayElementAtIndex(i).FindPropertyRelative("first").stringValue;
+                yield return name;
+            }
+        }
     }
 }
