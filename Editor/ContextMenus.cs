af084f32bcb452884c0a2550ec1b1249b9cedadd
anatawa12 <anatawa12@icloud.com>
2025-02-22 18:46:58 +0900

    chore: disable create menu when multiple objects are selected

--- Editor/ContextMenus.cs
+++ Editor/ContextMenus.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -30,16 +31,27 @@ namespace Anatawa12.AvatarOptimizer
             EditorGUIUtility.PingObject(newGameObject);
         }
 
+        // for GameObject/ menu, the handler will be called multiple times (selection count times)
+        //   so we need to filter it
+        // https://issuetracker.unity3d.com/issues/menuitem-is-executed-more-than-once-when-multiple-objects-are-selected
+        private static bool _called;
+        private static void OnceFilter(Action action)
+        {
+            if (_called) return;
+            action();
+            _called = true;
+            EditorApplication.delayCall += () => _called = false;
+        }
 
         [MenuItem(BASE_PATH + MERGE_SKINNED_MESH, true, PRIORITY)]
-        private static bool ValidateCreateMergeSkinnedMesh() => Selection.activeGameObject != null;
+        private static bool ValidateCreateMergeSkinnedMesh() => Selection.objects.Length == 1 && Selection.activeGameObject != null;
 
         [MenuItem(BASE_PATH + MERGE_SKINNED_MESH, false, PRIORITY)]
         private static void CreateMergeSkinnedMesh() => CreateAndAddComponent<MergeSkinnedMesh>(MERGE_SKINNED_MESH);
 
 
         [MenuItem(BASE_PATH + MERGE_PHYSBONE, true, PRIORITY)]
-        private static bool ValidateCreateMergePhysBone() => Selection.activeGameObject != null;
+        private static bool ValidateCreateMergePhysBone() => Selection.objects.Length == 1 && Selection.activeGameObject != null;
 
 #if AAO_VRCSDK3_AVATARS
         [MenuItem(BASE_PATH + MERGE_PHYSBONE, false, PRIORITY)]
