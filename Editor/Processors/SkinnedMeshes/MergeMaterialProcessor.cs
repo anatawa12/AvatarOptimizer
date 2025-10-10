bf6cc2aa42c58b632b3281ede1cabe28a405aba7
anatawa12 <anatawa12@icloud.com>
2025-10-10 10:05:40 +0900

    feat: initial implementation of MergeMaterial

--- Editor/Processors/SkinnedMeshes/MergeMaterialProcessor.cs
+++ Editor/Processors/SkinnedMeshes/MergeMaterialProcessor.cs
@@ -13,13 +13,13 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
     internal class MergeMaterialProcessor : EditSkinnedMeshProcessor<MergeMaterial>
     {
         private static readonly int MainTexProp = Shader.PropertyToID("_MainTex");
-        private static readonly int MainTexStProp = Shader.PropertyToID("_MainTex_ST");
+        private static readonly int MainTexTransformProp = Shader.PropertyToID("_MainTexTransform");
         private static readonly int RectProp = Shader.PropertyToID("_Rect");
 
         private static Material? _helperMaterial;
 
         private static Material HelperMaterial =>
-            _helperMaterial != null ? _helperMaterial : _helperMaterial = new Material(Assets.MergeTextureHelper);
+            _helperMaterial != null ? _helperMaterial : _helperMaterial = new Material(Assets.MergeTextureHelperV2);
 
         public MergeMaterialProcessor(MergeMaterial component) : base(component)
         {
@@ -27,13 +27,199 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
 
         public override EditSkinnedMeshProcessorOrder ProcessOrder => EditSkinnedMeshProcessorOrder.AfterRemoveMesh;
 
+        class ValidatedMergeInfo
+        {
+            public readonly Material ReferenceMaterial;
+            public readonly MaterialInformation ReferenceInformation;
+            public readonly MergeMaterial.MergeInfo Settings;
+            public readonly List<ValidatedMergeSource> Sources;
+
+            public ValidatedMergeInfo(Material referenceMaterial, 
+                MaterialInformation referenceInfomration,
+                MergeMaterial.MergeInfo settings,
+                List<ValidatedMergeSource> sources)
+            {
+                ReferenceMaterial = referenceMaterial;
+                ReferenceInformation = referenceInfomration;
+                Settings = settings;
+                Sources = sources;
+            }
+        }
+
+        class ValidatedMergeSource
+        {
+            public MergeMaterial.MergeSource Settings;
+            public int SubMeshIndex;
+            public List<TextureUsageInformation> Usages;
+            public Dictionary<string, TextureUsageInformation> UsagesByName;
+
+            public ValidatedMergeSource(MergeMaterial.MergeSource settings, int subMeshIndex, List<TextureUsageInformation> usages)
+            {
+                Settings = settings;
+                SubMeshIndex = subMeshIndex;
+                Usages = usages;
+                UsagesByName = usages.ToDictionary(x => x.MaterialPropertyName);
+            }
+        }
+
         public override void Process(BuildContext context, MeshInfo2 target)
         {
-#if false
-            // compute usages. AdditionalTemporal is usage count for now.
-            // if #usages is not zero for merging triangles
-            var users = new Dictionary<Vertex, int>();
+            // check and remove duplicated materials in merge setting
+            {
+                var mergeInfoByMaterial = new Dictionary<ObjectReference, MergeMaterial.MergeInfo>();
+                var duplicatedMaterials = new HashSet<ObjectReference>();
 
+                foreach (var componentMerge in Component.merges)
+                {
+                    var newSources = new List<MergeMaterial.MergeSource>(componentMerge.source.Length);
+                    foreach (var mergeSource in componentMerge.source)
+                    {
+                        if (mergeSource.material == null) continue;
+                        var materialReference = ObjectRegistry.GetReference(mergeSource.material);
+                        if (mergeInfoByMaterial.TryAdd(materialReference, componentMerge))
+                            newSources.Add(mergeSource);
+                        else
+                            duplicatedMaterials.Add(materialReference);
+                    }
+                    componentMerge.source = newSources.ToArray();
+                }
+
+                if (duplicatedMaterials.Count > 0)
+                    BuildLog.LogError("MergeMaterial:DuplicateMaterialInMergeSetting", duplicatedMaterials);
+            }
+
+            var submeshByMaterial = new Dictionary<ObjectReference, List<int>>();
+            for (var i = 0; i < target.SubMeshes.Count; i++)
+            {
+                var subMesh = target.SubMeshes[i];
+                foreach (var material in subMesh.SharedMaterials)
+                {
+                    var materialReference = ObjectRegistry.GetReference(material);
+                    if (!submeshByMaterial.TryGetValue(materialReference, out var list))
+                        submeshByMaterial.Add(materialReference, list = new List<int>());
+                    list.Add(i);
+                }
+            }
+
+            var slotsForMerge = new BitArray(target.SubMeshes.Count);
+            var validatedSettings = new List<ValidatedMergeInfo>();
+
+            foreach (var mergeInfo in Component.merges)
+            {
+                if (mergeInfo.source.Length == 0) continue;
+
+                // TODO: add option to override material properties and shader
+                Shader? shader = null;
+                Material? referenceMaterial = null;
+                MaterialInformation? referenceInfomration = null;
+                bool isAutoReferenceMaterial = referenceMaterial == null;
+                bool hasShaderError = false;
+                var goodSources = new List<ValidatedMergeSource>();
+
+                foreach (var mergeSource in mergeInfo.source)
+                {
+                    var materialReference = ObjectRegistry.GetReference(mergeSource.material);
+                    if (!submeshByMaterial.TryGetValue(materialReference, out var subMeshIndices))
+                    {
+                        BuildLog.LogError("MergeMaterial:MaterialNotFoundInMesh", materialReference);
+                        continue;
+                    }
+
+                    if (subMeshIndices.Count != 1)
+                    {
+                        BuildLog.LogError("MergeMaterial:MaterialUsedInMultipleSubMeshes",
+                            materialReference);
+                    }
+
+                    var subMesh = target.SubMeshes[subMeshIndices[0]];
+                    if (subMesh.SharedMaterials.Length != 1)
+                    {
+                        BuildLog.LogError("MergeMaterial:MultiMaterialSubMesh",
+                            materialReference);
+                    }
+
+                    var material = subMesh.SharedMaterial!;
+                    shader ??= material.shader;
+                    referenceMaterial ??= material;
+                    if (shader != material.shader)
+                    {
+                        BuildLog.LogError("MergeMaterial:DifferentShaderInMergeSetting",
+                            string.Join(", ", mergeInfo.source.Select(s => s.material?.name ?? "null")),
+                            mergeInfo.source.Select(x => x.material));
+                        continue;
+                    }
+
+                    if (hasShaderError) continue;
+
+                    // We don't reuse context.GetMaterialInformation(material)
+                    // because we need to get non-animated material information
+                    var matInfo = new MaterialInformation(material, new List<Renderer>() { Target }, null);
+                    referenceInfomration ??= matInfo;
+                    if (matInfo.DefaultResult is not { } shaderInformationResult)
+                    {
+                        BuildLog.LogError("MergeMaterial:UnsupportedShaderInMergeSetting", shader);
+                        hasShaderError = true;
+                        continue;
+                    }
+
+                    if (shaderInformationResult.TextureUsageInformationList is not { } textureUsageInformations)
+                    {
+                        BuildLog.LogError("MergeMaterial:UnsupportedShaderInMergeSetting", material);
+                        hasShaderError = true;
+                        continue;
+                    }
+
+                    var badProperties = textureUsageInformations.Where(usage => usage.UVChannel != UVChannel.NonMeshRelated && usage.UVMatrix == null)
+                        .Select(x => x.MaterialPropertyName)
+                        .ToList();
+
+                    if (badProperties.Any())
+                    {
+                        BuildLog.LogError("MergeMaterial:UnsupportedMaterialSettings:UnknownUVTransform", string.Join(", ", badProperties), material);
+                        continue;
+                    }
+
+                    goodSources.Add(new (mergeSource, subMeshIndices[0], textureUsageInformations));
+                }
+
+                if (!hasShaderError && referenceMaterial != null)
+                {
+                    // Check the textureUsage for referenceMaterial:
+                    // - has texture usage for all textures available
+                    // - has texture usage with identity matrix
+
+                    Utils.Assert(referenceInfomration!.Material == referenceMaterial);
+
+                    var allProperties = goodSources.SelectMany(x => x.Usages).Select(x => x.MaterialPropertyName).ToHashSet();
+                    var referenceUsages = referenceInfomration.DefaultResult!.TextureUsageInformationList!;
+                    if (!referenceUsages.Select(x => x.MaterialPropertyName).ToHashSet().SetEquals(allProperties))
+                    {
+                        // TODO: consider just ignore unused textures?
+                        var nonUsedProperties = allProperties.Except(referenceUsages.Select(x => x.MaterialPropertyName)).ToList();
+                        BuildLog.LogError("MergeMaterial:ReferenceMaterial:NotAllTexturesUsed", referenceMaterial, string.Join(", ", nonUsedProperties));
+                        continue;
+                    }
+
+                    if (referenceUsages.Any(usage => usage.UVChannel != UVChannel.NonMeshRelated && usage.UVMatrix == null))
+                    {
+                        var badProperties = referenceUsages.Where(usage => usage.UVChannel != UVChannel.NonMeshRelated && usage.UVMatrix == null)
+                            .Select(x => x.MaterialPropertyName)
+                            .ToList();
+
+                        // We don't have to create error if the reference material is automatically selected since it should
+                        // already errored
+                        if (!isAutoReferenceMaterial) BuildLog.LogError("MergeMaterial:ReferenceMaterial:UnknownUVTransform", string.Join(", ", badProperties), referenceMaterial);
+                        continue;
+                    }
+
+                    // The settings LGTM! let's prepare for merge.
+
+                    foreach (var goodSource in goodSources) slotsForMerge[goodSource.SubMeshIndex] = true;
+                    validatedSettings.Add(new ValidatedMergeInfo(referenceMaterial, referenceInfomration, mergeInfo, goodSources));
+                }
+            }
+
+            var users = new Dictionary<Vertex, int>();
             foreach (var v in target.Vertices) users[v] = 0;
 
             foreach (var targetSubMesh in target.SubMeshes)
@@ -41,16 +227,15 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
                 users[v]++;
 
             // compute per-material data
-            var mergingIndices = ComputeMergingIndices(target.SubMeshes.Count);
             var targetRectForMaterial = new Rect[target.SubMeshes.Count];
-            foreach (var componentMerge in Component.merges)
-            foreach (var mergeSource in componentMerge.source)
-                targetRectForMaterial[mergeSource.materialIndex] = mergeSource.targetRect;
-
-            // map UVs
+            foreach (var componentMerge in validatedSettings)
+            foreach (var mergeSource in componentMerge.Sources)
+                targetRectForMaterial[mergeSource.SubMeshIndex] = mergeSource.Settings.targetRect;
+            
+            // map UVs`
             for (var subMeshI = 0; subMeshI < target.SubMeshes.Count; subMeshI++)
             {
-                if (mergingIndices[subMeshI])
+                if (slotsForMerge[subMeshI])
                 {
                     // the material is for merge.
                     var subMesh = target.SubMeshes[subMeshI];
@@ -86,15 +271,13 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
 
             // merge submeshes
             target.FlattenMultiPassRendering("Merge Toon Lit");
-            var copied = target.SubMeshes.Where((_, i) => !mergingIndices[i]);
-            var materials = target.SubMeshes.Select(x => x.SharedMaterial).ToArray();
-            var merged = Component.merges.Select(x => new SubMesh(
-                x.source.SelectMany(src => target.SubMeshes[src.materialIndex].Triangles).ToList(),
-                CreateMaterial(GenerateTexture(x, materials, true))));
+            var copied = target.SubMeshes.Where((_, i) => !slotsForMerge[i]);
+            var merged = validatedSettings.Select(x => new SubMesh(
+                x.Sources.SelectMany(src => target.SubMeshes[src.SubMeshIndex].Triangles).ToList(),
+                CreateMaterial(x, target, compress: true)));
             var subMeshes = copied.Concat(merged).ToList();
             target.SubMeshes.Clear();
             target.SubMeshes.AddRange(subMeshes);
-#endif
         }
 
         private Vector2 MapUV(Vector2 vector2, Rect destSourceRect) =>
@@ -104,44 +287,17 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        /// <summary>
-        /// </summary>
-        /// <returns>bitarray[i] is true if the materials[i] ill be merged to other material</returns>
-        public BitArray ComputeMergingIndices(int subMeshCount)
+        private static Material CreateMaterial(ValidatedMergeInfo mergeInfo, MeshInfo2 target, bool compress)
         {
-            var mergingIndices = new BitArray(subMeshCount);
-#if false
-            foreach (var mergeInfo in Component.merges)
-            foreach (var source in mergeInfo.source)
-                mergingIndices[source.materialIndex] = true;
-#endif
-            return mergingIndices;
-        }
-
-        private Material?[] CreateMaterials(BitArray mergingIndices, Material?[] upstream, bool fast)
-        {
-            var copied = upstream.Where((_, i) => !mergingIndices[i]);
-            if (fast)
+            var mat = new Material(mergeInfo.ReferenceMaterial);
+            mat.name = "AAO Merged Material";
+            foreach (var information in mergeInfo.ReferenceInformation.DefaultResult!.TextureUsageInformationList!)
             {
-                return copied.Concat(Component.merges.Select(x => new Material(Assets.ToonLitShader))).ToArray();
+                var texture = GenerateTexture(mergeInfo, target.SubMeshes, information.MaterialPropertyName, compress);
+                texture.name = "AAO Merged Texture (for " + information.MaterialPropertyName + ")";
+                mat.SetTexture(information.MaterialPropertyName, texture);
             }
-            else
-            {
-                // slow mode: generate texture actually
-                return copied.Concat(GenerateTextures(Component, upstream, false).Select(CreateMaterial)).ToArray();
-            }
-        }
-
-        private static Material CreateMaterial(Texture texture)
-        {
-            var mat = new Material(Assets.ToonLitShader);
-            mat.SetTexture(MainTexProp, texture);
             return mat;
-        }
-
-        public static Texture[] GenerateTextures(MergeMaterial config, Material?[] materials, bool compress)
-        {
-            return config.merges.Select(x => GenerateTexture(x, materials, compress)).ToArray();
         }
 
         private static TextureFormat BaseTextureFormat(MergeMaterial.MergedTextureFormat finalFormat)
@@ -257,14 +413,15 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
         }
 
         private static Texture GenerateTexture(
-            MergeMaterial.MergeInfo mergeInfo,
-            Material?[] materials,
+            ValidatedMergeInfo mergeInfo,
+            List<SubMesh> subMeshes,
+            string propertyName,
             bool compress
         )
         {
-            var texWidth = mergeInfo.textureSize.x;
-            var texHeight = mergeInfo.textureSize.y;
-            var finalFormat = mergeInfo.mergedFormat;
+            var texWidth = mergeInfo.Settings.textureSize.x;
+            var texHeight = mergeInfo.Settings.textureSize.y;
+            var finalFormat = mergeInfo.Settings.mergedFormat;
             if (finalFormat == MergeMaterial.MergedTextureFormat.Default)
 #if UNITY_ANDROID || UNITY_IOS
                 finalFormat = MergeMaterial.MergedTextureFormat.ASTC_6x6;
@@ -277,17 +434,29 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
             targetFormat = SystemInfo.GetCompatibleFormat(targetFormat, FormatUsage.Render);
             var target = new RenderTexture(texWidth, texHeight, 0, targetFormat);
 
-#if falas
-            foreach (var source in mergeInfo.source)
+#if true
+            foreach (var source in mergeInfo.Sources)
             {
-                var sourceMat = materials[source.materialIndex]!; // selected material should not be null
-                var sourceTex = sourceMat.GetTexture(MainTexProp);
-                var sourceTexSt = sourceMat.GetVector(MainTexStProp);
+                if (!source.UsagesByName.TryGetValue(propertyName, out var usageInformation)) continue;
+                if (usageInformation.UVChannel == UVChannel.NonMeshRelated) continue;
+                var sourceMat = subMeshes[source.SubMeshIndex].SharedMaterial!; // selected material should not be null
+                var sourceTex = sourceMat.GetTexture(propertyName);
+                var sourceTexTransform = usageInformation.UVMatrix!.Value;
                 HelperMaterial.SetTexture(MainTexProp, sourceTex);
-                HelperMaterial.SetVector(MainTexStProp, sourceTexSt);
+                HelperMaterial.SetMatrix(MainTexTransformProp, new UnityEngine.Matrix4x4()
+                {
+                    m00 = sourceTexTransform.M00,
+                    m01 = sourceTexTransform.M01,
+                    m02 = sourceTexTransform.M02,
+                    m10 = sourceTexTransform.M10,
+                    m11 = sourceTexTransform.M11,
+                    m12 = sourceTexTransform.M12,
+                    m22 = 1,
+                    m33 = 1,
+                });
                 HelperMaterial.SetVector(RectProp,
-                    new Vector4(source.targetRect.x, source.targetRect.y, source.targetRect.width,
-                        source.targetRect.height));
+                    new Vector4(source.Settings.targetRect.x, source.Settings.targetRect.y, 
+                        source.Settings.targetRect.width, source.Settings.targetRect.height));
                 Graphics.Blit(sourceTex, target, HelperMaterial);
             }
 #endif
@@ -336,7 +505,8 @@ namespace Anatawa12.AvatarOptimizer.Processors.SkinnedMeshes
             public override Material?[] Materials(bool fast = true)
             {
                 var upstream = base.Materials(fast);
-                return _processor.CreateMaterials(_processor.ComputeMergingIndices(upstream.Length), upstream, fast);
+                // TODO: IMPLEMENT THIS FUNCTION
+                return upstream;
             }
         }
     }
