e623013e40c642b79640d3b88fa3c8cd94aee0d3
anatawa12 <anatawa12@icloud.com>
2025-11-09 03:33:14 +0900

    fix: basic mesh support changes

--- Editor/Processors/MaxTextureSizeProcessor.cs
+++ Editor/Processors/MaxTextureSizeProcessor.cs
@@ -46,9 +46,9 @@ namespace Anatawa12.AvatarOptimizer.Processors
                 IEnumerable<Material> materials;
 
                 // Use MeshInfo2 materials for SkinnedMeshRenderer
-                if (renderer is SkinnedMeshRenderer skinnedMesh)
+                var meshInfo = context.TryGetMeshInfoFor(renderer);
+                if (meshInfo != null)
                 {
-                    var meshInfo = context.GetMeshInfoFor(skinnedMesh);
                     materials = meshInfo.SubMeshes.SelectMany(x => x.SharedMaterials).Where(m => m != null);
                 }
                 else
