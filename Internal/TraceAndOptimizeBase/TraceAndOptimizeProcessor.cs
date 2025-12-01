5bfa9046b3ad98823c7acbfabd313f532b93f0bb
anatawa12 <anatawa12@icloud.com>
2025-12-02 00:21:50 +0900

    feat: initial commit for per-platform configuration

--- Internal/TraceAndOptimizeBase/TraceAndOptimizeProcessor.cs
+++ Internal/TraceAndOptimizeBase/TraceAndOptimizeProcessor.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using nadena.dev.ndmf;
+using UnityEditor;
 using UnityEngine;
+using static Anatawa12.AvatarOptimizer.TraceAndOptimizePlatformSettings;
 
 namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
 {
@@ -53,7 +56,7 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
         public Dictionary<SkinnedMeshRenderer, HashSet<string>> PreserveBlendShapes =
             new Dictionary<SkinnedMeshRenderer, HashSet<string>>();
 
-        internal void Initialize(TraceAndOptimize config)
+        internal void Initialize(TraceAndOptimize config, TraceAndOptimizePlatformSettings settings)
         {
             // optimization settings
             AllowShuffleMaterialSlots = config.allowShuffleMaterialSlots;
@@ -63,50 +66,50 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
             Exclusions = new HashSet<GameObject?>(config.debugOptions.exclusions ?? Array.Empty<GameObject?>());
             GCDebug = (int)config.debugOptions.gcDebug;
 
-            if (config.optimizeBlendShape)
+            if (config.optimizeBlendShape && settings.optimizeBlendShape)
             {
-                FreezingNonAnimatedBlendShape = !config.debugOptions.skipFreezingNonAnimatedBlendShape;
-                FreezingMeaninglessBlendShape = !config.debugOptions.skipFreezingMeaninglessBlendShape;
-                AutoMergeBlendShape = !config.debugOptions.skipAutoMergeBlendShape;
+                FreezingNonAnimatedBlendShape = !config.debugOptions.skipFreezingNonAnimatedBlendShape && settings.freezingNonAnimatedBlendShape;
+                FreezingMeaninglessBlendShape = !config.debugOptions.skipFreezingMeaninglessBlendShape && settings.freezingMeaninglessBlendShape;
+                AutoMergeBlendShape = !config.debugOptions.skipAutoMergeBlendShape && settings.autoMergeBlendShape;
             }
 
-            if (config.removeUnusedObjects)
+            if (config.removeUnusedObjects && settings.removeUnusedObjects)
             {
-                SweepComponents = !config.debugOptions.noSweepComponents;
-                ConfigureLeafMergeBone = !config.debugOptions.noConfigureLeafMergeBone;
-                ConfigureMiddleMergeBone = !config.debugOptions.noConfigureMiddleMergeBone;
-                ActivenessAnimation = !config.debugOptions.noActivenessAnimation;
-                RemoveEmptySubMesh = !config.debugOptions.skipRemoveEmptySubMesh;
-                RemoveMaterialUnusedProperties = !config.debugOptions.skipRemoveMaterialUnusedProperties;
-                RemoveMaterialUnusedTextures = !config.debugOptions.skipRemoveMaterialUnusedTextures;
-                MergeMaterials = !config.debugOptions.skipMergeMaterials;
+                SweepComponents = !config.debugOptions.noSweepComponents && settings.sweepComponents;
+                ConfigureLeafMergeBone = !config.debugOptions.noConfigureLeafMergeBone && settings.configureLeafMergeBone;
+                ConfigureMiddleMergeBone = !config.debugOptions.noConfigureMiddleMergeBone && settings.configureMiddleMergeBone;
+                ActivenessAnimation = !config.debugOptions.noActivenessAnimation && settings.activenessAnimation;
+                RemoveEmptySubMesh = !config.debugOptions.skipRemoveEmptySubMesh && settings.removeEmptySubMesh;
+                RemoveMaterialUnusedProperties = !config.debugOptions.skipRemoveMaterialUnusedProperties && settings.removeMaterialUnusedProperties;
+                RemoveMaterialUnusedTextures = !config.debugOptions.skipRemoveMaterialUnusedTextures && settings.removeMaterialUnusedTextures;
+                MergeMaterials = !config.debugOptions.skipMergeMaterials && settings.mergeMaterials;
             }
 
-            if (config.optimizePhysBone)
+            if (config.optimizePhysBone && settings.optimizePhysBone)
             {
-                IsAnimatedOptimization = !config.debugOptions.skipIsAnimatedOptimization;
-                MergePhysBoneCollider = !config.debugOptions.skipMergePhysBoneCollider;
-                ReplaceEndBoneWithEndpointPosition = !config.debugOptions.skipReplaceEndBoneWithEndpointPosition;
-                MergePhysBones = !config.debugOptions.skipMergePhysBones;
+                IsAnimatedOptimization = !config.debugOptions.skipIsAnimatedOptimization && settings.isAnimatedOptimization;
+                MergePhysBoneCollider = !config.debugOptions.skipMergePhysBoneCollider && settings.mergePhysBoneCollider;
+                ReplaceEndBoneWithEndpointPosition = !config.debugOptions.skipReplaceEndBoneWithEndpointPosition && settings.replaceEndBoneWithEndpointPosition;
+                MergePhysBones = !config.debugOptions.skipMergePhysBones && settings.mergePhysBones;
             }
 
-            if (config.removeZeroSizedPolygons)
+            if (config.removeZeroSizedPolygons && settings.removeZeroSizedPolygons)
             {
                 RemoveZeroSizedPolygon = true;
             }
 
-            if (config.optimizeAnimator)
+            if (config.optimizeAnimator && settings.optimizeAnimator)
             {
                 OptimizeAnimator = true;
-                EntryExitToBlendTree = !config.debugOptions.skipEntryExitToBlendTree;
-                RemoveUnusedAnimatingProperties = !config.debugOptions.skipRemoveUnusedAnimatingProperties;
-                MergeBlendTreeLayer = !config.debugOptions.skipMergeBlendTreeLayer;
-                RemoveMeaninglessAnimatorLayer = !config.debugOptions.skipRemoveMeaninglessAnimatorLayer;
-                AnyStateToEntryExit = !config.debugOptions.skipAnyStateToEntryExit;
-                CompleteGraphToEntryExit = !config.debugOptions.skipCompleteGraphToEntryExit;
+                EntryExitToBlendTree = !config.debugOptions.skipEntryExitToBlendTree && settings.entryExitToBlendTree;
+                RemoveUnusedAnimatingProperties = !config.debugOptions.skipRemoveUnusedAnimatingProperties && settings.removeUnusedAnimatingProperties;
+                MergeBlendTreeLayer = !config.debugOptions.skipMergeBlendTreeLayer && settings.mergeBlendTreeLayer;
+                RemoveMeaninglessAnimatorLayer = !config.debugOptions.skipRemoveMeaninglessAnimatorLayer && settings.removeMeaninglessAnimatorLayer;
+                AnyStateToEntryExit = !config.debugOptions.skipAnyStateToEntryExit && settings.anyStateToEntryExit;
+                CompleteGraphToEntryExit = !config.debugOptions.skipCompleteGraphToEntryExit && settings.completeGraphToEntryExit;
             }
 
-            if (config.mergeSkinnedMesh)
+            if (config.mergeSkinnedMesh && settings.mergeSkinnedMesh)
             {
                 MergeSkinnedMesh = true;
                 MergeStaticSkinnedMesh = !config.debugOptions.skipMergeStaticSkinnedMesh;
@@ -114,7 +117,7 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
                 MergeMaterialAnimatingSkinnedMesh = !config.debugOptions.skipMergeMaterialAnimatingSkinnedMesh;
             }
 
-            if (config.optimizeTexture)
+            if (config.optimizeTexture && settings.optimizeTexture)
             {
                 OptimizeTexture = true;
             }
@@ -132,9 +135,71 @@ namespace Anatawa12.AvatarOptimizer.Processors.TraceAndOptimizes
         protected override void Execute(BuildContext context)
         {
             var config = context.AvatarRootObject.GetComponent<TraceAndOptimize>();
+            TraceAndOptimizePlatformSettings settings = LoadPlatformSettings(context.PlatformProvider.QualifiedName,
+                context.PlatformProvider.DisplayName);
+
             if (config)
-                context.GetState<TraceAndOptimizeState>().Initialize(config);
+            {
+                context.GetState<TraceAndOptimizeState>().Initialize(config, settings);
+            }
+
             DestroyTracker.DestroyImmediate(config);
+        }
+
+        private static TraceAndOptimizePlatformSettings? _full;
+
+        private static TraceAndOptimizePlatformSettings LoadPlatformSettings(
+            string platformQualifiedName, string displayName)
+        {
+            if (_full == null) _full = ScriptableObject.CreateInstance<TraceAndOptimizePlatformSettings>();
+            //if (platformQualifiedName == WellKnownPlatforms.VRChatAvatar30) return _full;
+            if (platformQualifiedName == WellKnownPlatforms.Generic)
+            {
+                BuildLog.LogInfo("NonVRChatPlatformSupport:genericPlatformMessage");
+                return _full;
+            }
+
+            var settings = AssetDatabase.FindAssets("t:TraceAndOptimizePlatformSettings")
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<TraceAndOptimizePlatformSettings>)
+                .Where(x => x != null)
+                .Where(x => x.platformQualifiedName == platformQualifiedName)
+                .ToList();
+
+            settings.Sort((a, b) =>
+            {
+                var aDiff = Math.Abs(a!.experimentalPlatformSettingsVersion - CurrentExperimentalPlatformSettingsVersion);
+                var bDiff = Math.Abs(b!.experimentalPlatformSettingsVersion - CurrentExperimentalPlatformSettingsVersion);
+                return aDiff.CompareTo(bDiff);
+            });
+
+            BuildLog.LogWarning("NonVRChatPlatformSupport:experimentalMessage");
+
+            using var enumerator = settings.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                BuildLog.LogWarning("NonVRChatPlatformSupport:noPlatformSettings", displayName, platformQualifiedName);
+                return _full;
+            }
+
+            var result = enumerator.Current!;
+
+            if (enumerator.MoveNext())
+            {
+                BuildLog.LogWarning("NonVRChatPlatformSupport:manyPlatformSettings", displayName, platformQualifiedName,
+                    AssetDatabase.GetAssetPath(result));
+            }
+
+            if (result.experimentalPlatformSettingsVersion != CurrentExperimentalPlatformSettingsVersion)
+            {
+                BuildLog.LogWarning("NonVRChatPlatformSupport:versionMismatchMessage:build",
+                    AssetDatabase.GetAssetPath(result),
+                    result!.experimentalPlatformSettingsVersion, 
+                    CurrentExperimentalPlatformSettingsVersion);
+            }
+
+            return result!;
         }
     }
 
