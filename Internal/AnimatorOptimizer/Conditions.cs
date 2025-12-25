b5ec3b3050719b7d63651612009f93a088416d6d
anatawa12 <anatawa12@icloud.com>
2025-12-26 05:43:30 +0900

    fix: complete graph to entry exit broke immediate transition
    The issue is miminoko v2 hard mode is broken.
    This is because miminoko v2 uses 'always true' condition to enter a state for single frame, but C2E/E will transform to A>A rather than A>B>A.
    This commit fixes this by denying transforming C2E/E for possible immediate transition.
    This may reduce the optimization target cases, but the main target of this optimization; C2E/E2BlendTree does not affected by this change

---
+++ Internal/AnimatorOptimizer/Conditions.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Anatawa12.AvatarOptimizer.Processors.AnimatorOptimizer;
+
+public static class Conditions
+{
+    public static bool IsContradiction(
+        // AnimatorCondition[AND]
+        IEnumerable<AnimatorCondition> conditions,
+        Dictionary<string, AnimatorControllerParameterType> typeByName)
+    {
+        var conditionsByParameter = conditions.GroupBy(c => c.parameter);
+
+        foreach (var group in conditionsByParameter)
+        {
+            var parameterName = group.Key;
+            var parameterType = typeByName[parameterName];
+
+            var parameterConds = group.ToArray();
+            bool isContradiction = parameterType switch
+            {
+                AnimatorControllerParameterType.Float =>
+                    RangesUtil.FloatRangeSetFromConditions(parameterConds).IsEmpty(),
+                AnimatorControllerParameterType.Int =>
+                    RangesUtil.IntRangeSetFromConditions(parameterConds).IsEmpty(),
+                AnimatorControllerParameterType.Bool =>
+                    RangesUtil.BoolSetFromConditions(parameterConds).IsEmpty(),
+                AnimatorControllerParameterType.Trigger =>
+                    RangesUtil.BoolSetFromConditions(parameterConds).IsEmpty(),
+                _ => throw new ArgumentOutOfRangeException(),
+            };
+
+            if (isContradiction) return true;
+        }
+
+        return false;
+    }
+}
