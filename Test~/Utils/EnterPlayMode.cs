4ba1f1eb5744c089915c6a5b6deef50e413bd3b0
anatawa12 <anatawa12@icloud.com>
2026-08-13 00:02:39 +0900

    test: prevent freezeing when enterplaymode settings are not in expected state

---
+++ Test~/Utils/EnterPlayMode.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.TestTools;
+
+namespace Anatawa12.AvatarOptimizer.Test
+{
+    public class EnterPlayMode : IEditModeTestYieldInstruction
+    {
+        private UnityEngine.TestTools.EnterPlayMode _inner;
+
+        public EnterPlayMode()
+        {
+            if (!EditorSettings.enterPlayModeOptionsEnabled
+                || (EditorSettings.enterPlayModeOptions & EnterPlayModeOptions.DisableDomainReload) == 0
+                )
+            {
+                throw new Exception("EnterPlayMode requires EnterPlayModeOptions enabled and enter play mode disabled");
+            }
+            _inner = new UnityEngine.TestTools.EnterPlayMode(expectDomainReload: false);
+        }
+
+        public IEnumerator Perform() => _inner.Perform();
+
+        public bool ExpectDomainReload => _inner.ExpectDomainReload;
+        public bool ExpectedPlaymodeState => _inner.ExpectedPlaymodeState;
+    }
+}
