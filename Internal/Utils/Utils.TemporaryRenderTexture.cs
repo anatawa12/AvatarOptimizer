92f5c34cde5ac0f0479dff95dd8195965a586445
anatawa12 <anatawa12@icloud.com>
2026-08-12 15:31:35 +0900

    fix: remove unused methods

--- Internal/Utils/Utils.TemporaryRenderTexture.cs
+++ Internal/Utils/Utils.TemporaryRenderTexture.cs
@@ -1,63 +1,10 @@
 using System;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
-#if UNITY_6000_0_OR_NEWER
-using FormatUsage = UnityEngine.Experimental.Rendering.GraphicsFormatUsage;
-#endif
 namespace Anatawa12.AvatarOptimizer;
 
 partial class Utils
 {
-    private static TemporaryRenderTextureScope TemporaryRenderTextureImpl(
-        int width,
-        int height,
-        GraphicsFormat depthStencilFormat,
-        GraphicsFormat colorFormat,
-        int antiAliasing = 1,
-        RenderTextureMemoryless memorylessMode = RenderTextureMemoryless.None,
-        VRTextureUsage vrUsage = VRTextureUsage.None,
-        bool useDynamicScale = false)
-    {
-        return TemporaryRenderTexture(new RenderTextureDescriptor(width, height, colorFormat, depthStencilFormat)
-        {
-            msaaSamples = antiAliasing,
-            memoryless = memorylessMode,
-            vrUsage = vrUsage,
-            useDynamicScale = useDynamicScale
-        });
-    }
-
-    internal static GraphicsFormat GetDepthStencilFormatLegacy(
-        int depthBits,
-        RenderTextureFormat format)
-    {
-        return GetDepthStencilFormatLegacy(depthBits, format == RenderTextureFormat.Shadowmap);
-    }
-
-    internal static GraphicsFormat GetDepthStencilFormatLegacy(
-        int depthBits,
-        bool requestedShadowMap)
-    {
-        return requestedShadowMap ? GraphicsFormatUtility.GetDepthStencilFormat(depthBits, 0) : GraphicsFormatUtility.GetDepthStencilFormat(depthBits, 8);
-    }
-
-    public static TemporaryRenderTextureScope TemporaryRenderTexture(
-      int width,
-      int height,
-      int depthBuffer = 0,
-      RenderTextureFormat format = RenderTextureFormat.Default,
-      RenderTextureReadWrite readWrite = RenderTextureReadWrite.Default,
-      int antiAliasing = 1,
-      RenderTextureMemoryless memorylessMode = RenderTextureMemoryless.None,
-      VRTextureUsage vrUsage = VRTextureUsage.None,
-      bool useDynamicScale = false)
-    {
-        var compatibleFormat = GetCompatibleFormat(format, readWrite);
-        var stencilFormatLegacy = GetDepthStencilFormatLegacy(depthBuffer, format);
-
-        return TemporaryRenderTextureImpl(width, height, stencilFormatLegacy, compatibleFormat, antiAliasing, memorylessMode, vrUsage, useDynamicScale);
-    }
-
     public static TemporaryRenderTextureScope TemporaryRenderTexture(
       int width,
       int height,
@@ -68,19 +15,6 @@ partial class Utils
       VRTextureUsage vrUsage = VRTextureUsage.None,
       bool useDynamicScale = false)
     => new(RenderTexture.GetTemporary(width, height, depthBuffer, format, antiAliasing, memorylessMode, vrUsage, useDynamicScale));
-    private static GraphicsFormat GetCompatibleFormat(
-        RenderTextureFormat renderTextureFormat,
-        RenderTextureReadWrite readWrite)
-    {
-        var graphicsFormat = GraphicsFormatUtility.GetGraphicsFormat(renderTextureFormat, readWrite);
-        var compatibleFormat = SystemInfo.GetCompatibleFormat(graphicsFormat, FormatUsage.Render);
-        if (graphicsFormat == compatibleFormat)
-            return graphicsFormat;
-        return compatibleFormat;
-    }
-
-    public static TemporaryRenderTextureScope TemporaryRenderTexture(RenderTextureDescriptor descriptor) =>
-        new(RenderTexture.GetTemporary(descriptor));
 
     public struct TemporaryRenderTextureScope : IDisposable
     {
